20c8386f6c1772a7c8ad04b6fbb478a1c74d2beb
jwlin <cs.castman@gmail.com>
2023-09-30 11:55:30 -0700

    fix whitenoise storage name

--- dgpaproject/azure.py
+++ dgpaproject/azure.py
@@ -1,14 +1,6 @@
 from .settings import * 
 import os
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-
-if 'CUSTOM_DOMAIN' in os.environ:
-    ALLOWED_HOSTS += [os.environ['CUSTOM_DOMAIN']]
-    CSRF_TRUSTED_ORIGINS += ['https://' + os.environ['CUSTOM_DOMAIN']]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -29,8 +21,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/") 
 
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+#STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedStaticFilesStorage')
 
 DEBUG = True if os.environ.get('DEBUG') == "True" else False
 
 ADMIN_ENABLED = True if os.environ.get('ADMIN_ENABLED') == "True" else False
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+if 'CUSTOM_DOMAIN' in os.environ:
+    ALLOWED_HOSTS += [os.environ['CUSTOM_DOMAIN']]
+    CSRF_TRUSTED_ORIGINS += ['https://' + os.environ['CUSTOM_DOMAIN']]
