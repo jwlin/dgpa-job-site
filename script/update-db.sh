8730ee5899ab214bc5c0d3a6fc0e8cd007b563b6
Jun-Wei Lin <cs.castman@gmail.com>
2024-03-31 08:55:03 -0700

    Update update-db.sh

--- script/update-db.sh
+++ script/update-db.sh
@@ -4,21 +4,21 @@ base_dir=
 
 echo "[$(date)] Script starts"
 
-echo "Unzip."
+echo "[$(date)] Unzip."
 cd ${base_dir}/data/daily
 zip_file=$(ls *.zip | head -n 1)
 unzip ${zip_file} -d ${base_dir}/data/daily
 
-echo "Update DB with the XML file."
+echo "[$(date)] Update DB with the XML file."
 cd ${base_dir}/dgpa-job-site
-pipenv run python job/update_db.py
+pipenv run python job/update_db.py >> ${base_dir}/log/update_db.log 2>&1
 
-echo "Remove the XML file."
+echo "[$(date)] Remove the XML file."
 cd ${base_dir}/data/daily
 xml_file=$(ls *.xml | head -n 1)
 rm ${xml_file}
 
-echo "Move the zip file to archive."
+echo "[$(date)] Move the zip file to archive."
 mv ${zip_file} ${base_dir}/data/archive/
 
 echo "[$(date)] Script ends"
