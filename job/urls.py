915a9cf0edf047dc8fc3d50f142747b646047cab
jwlin <cs.castman@gmail.com>
2024-11-03 06:24:48 -0800

    disable commenting

--- job/urls.py
+++ job/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     re_path(r'^trend$', views.trend, name='trend'),
     re_path(r'^dept$', views.dept, name='dept'),
     re_path(r'^(?P<job_id>[0-9]+)$', views.item, name='item'),
-    re_path(r'^api/message/(?P<job_id>[0-9]+)$', views.message, name='message'),
+    # 2024-11-03: remove commenting feature
+    # re_path(r'^api/message/(?P<job_id>[0-9]+)$', views.message, name='message'),
     re_path(r'^api/dept/$', views.dept_ajax, name='dept_ajax'),
     re_path(r'api/trend/$', views.trend_ajax, name='trend_ajax'),
 ]
